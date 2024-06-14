0f86b9d14072e068b4a117eb28106840adc87e24
Bryan Corradino <zeromuseg7@gmail.com>
2024-06-14 21:00:37 +0200

    Refactor World so that it does not rely on the componentTypes array

--- src/main/scala/ecscalibur/core/World.scala
+++ src/main/scala/ecscalibur/core/World.scala
@@ -20,14 +20,12 @@ object Worlds:
     def apply(): World = WorldImpl()
 
   private class WorldImpl extends World:
-    private val componentTypes: ArrayBuffer[ArrayBuffer[ComponentId]] = ArrayBuffer.empty
     private val components: ArrayBuffer[ArrayBuffer[Component]] = ArrayBuffer.empty
     import ecscalibur.id.IdGenerator
     private val idGenerator = IdGenerator()
 
     override def spawn: Entity =
       val id = idGenerator.next
-      if !componentTypes.contains(id) then componentTypes += ArrayBuffer.empty
       if !components.contains(id) then components += ArrayBuffer.empty
       Entity(id)
 
@@ -37,13 +35,15 @@ object Worlds:
     override inline def addComponent[T <: Component](e: Entity, c: T) =
       if hasComponent(e, c.id) then throw IllegalStateException(s"Entity $this already has a component of type ${c.getClass}.")
       components(e.id) += c
-      componentTypes(e.id) += c.id
 
     override inline def removeComponent(e: Entity, id: ComponentId): Unit =
-      val i = componentTypes(e.id).indexOf(id)
+      val i = findComponentIdx(e, id) 
       if i == -1 then throw new IllegalStateException(s"Entity $this does not have a component of type ID $id.")
       components(e.id).remove(i)
-      componentTypes(e.id).remove(i)
 
     override inline def hasComponent(e: Entity, id: ComponentId): Boolean =
-      componentTypes(e.id).contains(id)
+      findComponentIdx(e, id) != -1
+
+    private inline def findComponentIdx(e: Entity, id: ComponentId): Int =
+      components(e.id).indexWhere(_.id == id)
+
