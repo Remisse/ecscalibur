129489439cbb51d51a7032d2fb5e7591ac2a7686
Bryan Corradino <zeromuseg7@gmail.com>
2024-06-17 23:20:09 +0200

    Add more methods to Archetype

--- src/main/scala/ecscalibur/core/archetype/Archetype.scala
+++ src/main/scala/ecscalibur/core/archetype/Archetype.scala
@@ -1,25 +1,74 @@
 package ecscalibur.core.archetype
 
-import ecscalibur.core.Components.ComponentType
-import scala.quoted.*
-import scala.collection.immutable.ArraySeq
+import ecscalibur.core.Components.{Component, ComponentId, ComponentType}
+import ecscalibur.core.Entities.Entity
+import ecscalibur.id.IdGenerator
+
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.immutable.*
 
 object Archetypes:
   trait Archetype:
     def hasSignature(types: ComponentType*): Boolean
-    def contains(types: ComponentType*): Boolean
+    def handles(types: ComponentType*): Boolean
+    def add(e: Entity, comps: Array[Component]): Unit
+    def contains(e: Entity): Boolean
+    def remove(e: Entity): Unit
+    def get(e: Entity, comp: ComponentType): Component
 
   object Archetype:
     def apply(types: ComponentType*): Archetype =
-      require(!types.isEmpty, "Given signature is empty.")
+      require(types.nonEmpty, "Given signature is empty.")
       val distinct = types.distinct
       require(distinct.length == types.length, "Given signature has duplicate component types.")
       ArchetypeImpl(distinct)
 
     private class ArchetypeImpl(types: Seq[ComponentType]) extends Archetype:
-      val signature: Array[ComponentType] = types.toArray.sorted
-      override inline def hasSignature(types: ComponentType*): Boolean =
-        signature.sameElements(types.toArray.sorted)
+      private val signature: Array[ComponentId] = types.map(~_).toArray.sorted
+      private val entityIndexes: mutable.HashMap[Entity, Int] = mutable.HashMap.empty
+      private val components: Map[ComponentId, ArrayBuffer[Component]] =
+        signature.view.map(t => t -> ArrayBuffer.empty[Component]).to(HashMap)
+      private val idGenerator: IdGenerator = IdGenerator()
 
-      override inline def contains(types: ComponentType*): Boolean =
-        signature.containsSlice(types.toArray.sorted)
+      override inline def hasSignature(types: ComponentType*): Boolean =
+        require(types.nonEmpty, "Given signature to test is empty.")
+        hasSignatureInternal(types.map(~_))
+
+      private inline def hasSignatureInternal(ids: Seq[ComponentId]): Boolean =
+        signature.sameElements(ids.sorted)
+
+      override inline def handles(types: ComponentType*): Boolean =
+        require(types.nonEmpty, "Given type sequence is empty.")
+        signature.containsSlice(types.map(~_).sorted)
+
+      override inline def add(e: Entity, comps: Array[Component]): Unit =
+        require(!entityIndexes.contains(e), "Attempted to readd an already existing entity.")
+        require(
+          hasSignatureInternal(comps.map(~_)),
+          "Given component types do not correspond to this archetype's signature."
+        )
+        val newEntityIdx = idGenerator.next
+        entityIndexes += e -> newEntityIdx
+        for c <- comps do
+          if newEntityIdx >= components(~c).length then components(~c) += c
+          else components(~c).update(newEntityIdx, c)
+
+      override inline def contains(e: Entity): Boolean = idGenerator.isValid(entityIndexes(e))
+
+      override def remove(e: Entity): Unit =
+        inline val errorMsg = "Attempted to remove an entity not stored in this archetype."
+        require(entityIndexes.contains(e), errorMsg)
+        val idx = entityIndexes(e)
+        require(idGenerator.isValid(idx), errorMsg)
+        idGenerator.erase(idx)
+
+      override inline def get(e: Entity, comp: ComponentType): Component =
+        require(contains(e), "Failed to find the given entity.")
+        require(handles(comp), "Given type is not part of this archetype's signature.")
+        components(~comp)(entityIndexes(e))
+
+  import ecscalibur.core.Components.Annotations.component
+  @component
+  private[Archetypes] class NilComponent extends Component
+  private[Archetypes] object NilComponent extends ComponentType
--- src/test/scala/ecscalibur/ArchetypeTest.scala
+++ src/test/scala/ecscalibur/ArchetypeTest.scala
@@ -24,17 +24,69 @@ class ArchetypeTest extends AnyFlatSpec with should.Matchers:
     an[IllegalArgumentException] shouldBe thrownBy(Archetype())
 
   "An archetype" should "be identified by the component classes it holds" in:
-    val archetype = Archetype(C1, C2)    
-    archetype.hasSignature(C1, C2) shouldBe true 
-    archetype.hasSignature(C2, C1) shouldBe true 
-    archetype.hasSignature(C1) shouldBe false 
+    val archetype = Archetype(C1, C2)
+    archetype.hasSignature(C1, C2) shouldBe true
+    archetype.hasSignature(C2, C1) shouldBe true
+    archetype.hasSignature(C1) shouldBe false
     archetype.hasSignature(C1, C2, C3) shouldBe false
 
   it should "report which component types it owns" in:
-    val archetype = Archetype(C1, C2)    
-    archetype.contains(C1) shouldBe true
-    archetype.contains(C2, C1) shouldBe true
-    archetype.contains(C3) shouldBe false
+    val archetype = Archetype(C1, C2)
+    archetype.handles(C1) shouldBe true
+    archetype.handles(C2, C1) shouldBe true
+    archetype.handles(C3) shouldBe false
 
   it should "have a signature made of distinct component types only" in:
     an[IllegalArgumentException] shouldBe thrownBy(Archetype(C1, C1, C2))
+
+  @component
+  case class WithValue(val x: Int) extends Component
+  object WithValue extends ComponentType
+
+  import ecscalibur.core.Worlds.World
+
+  it should "correctly store entities and their components" in:
+    given world: World = World()
+    val e1 = world.spawn
+    val archetype = Archetype(C1, C2)
+    archetype.add(e1, Array(C1(), C2()))
+    archetype.contains(e1) shouldBe true
+
+  it should "correctly return any component values associated to its entities" in:
+    given world: World = World()
+    val e1 = world.spawn
+    val e2 = world.spawn
+    val archetype = Archetype(WithValue, C2)
+    val c1 = WithValue(5)
+    val c2 = C2()
+    archetype.add(e1, Array(c1, c2))
+    archetype.add(e2, Array(c2, c1))
+    val returned = archetype.get(e1, WithValue) match
+      case c: WithValue => c
+      case _            => throw new IllegalStateException()
+    returned shouldBe c1 
+    archetype.get(e2, C2) shouldBe c2
+
+  it should "not accept entities that do not satisfy its signature" in:
+    given world: World = World()
+    val e1 = world.spawn
+    val archetype = Archetype(WithValue, C2)
+    an[IllegalArgumentException] shouldBe thrownBy(archetype.add(e1, Array(C2())))
+    an[IllegalArgumentException] shouldBe thrownBy(archetype.add(e1, Array(WithValue(1), C2(), C3())))
+
+  it should "correctly remove stored entities" in:
+    given world: World = World()
+    val e1 = world.spawn
+    val archetype = Archetype(C1)
+    archetype.add(e1, Array(C1()))
+    archetype.remove(e1)
+    archetype.contains(e1) shouldBe false
+  
+  it should "not return component values associated to deleted entities" in:
+    given world: World = World()
+    val e1 = world.spawn
+    val c1 = WithValue(1)
+    val archetype = Archetype(WithValue)
+    archetype.add(e1, Array(c1))
+    archetype.remove(e1)
+    an[IllegalArgumentException] shouldBe thrownBy (archetype.get(e1, WithValue))
