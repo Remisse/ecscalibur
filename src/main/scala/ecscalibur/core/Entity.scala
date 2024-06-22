aeadbe8bece4739143f45f98a90cc82a5e6ddf8c
Bryan Corradino <zeromuseg7@gmail.com>
2024-06-22 12:53:35 +0200

    Make Entity non-opaque

--- src/main/scala/ecscalibur/core/Entity.scala
+++ src/main/scala/ecscalibur/core/Entity.scala
@@ -4,31 +4,34 @@ import Components.*
 import Worlds.World
 import scala.annotation.targetName
 
+export Entities.Extensions.*
+
 object Entities:
-  opaque type Entity = Int
+  type Entity = Int
 
   /**
-    * Factory for [[ecscalibur.core.Entities.Entity]].
+    * Factory for [[Entity]].
     */
   private[core] object Entity:
     def apply(id: Int): Entity = id
 
-  extension (e: Entity)
-    inline def id: Int = e
+  object Extensions:
+    extension (e: Entity)
+      inline def id: Int = e
 
-    @targetName("add")
-    inline infix def +=[T <: Component](c: T)(using world: World): Entity =
-      world.addComponent(e, c)
-      e
+      @targetName("add")
+      inline infix def +=[T <: Component](c: T)(using world: World): Entity =
+        world.addComponent(e, c)
+        e
 
-    @targetName("remove")
-    inline infix def -=(compType: ComponentType)(using world: World): Entity =
-      world.removeComponent(e, compType)
-      e
+      @targetName("remove")
+      inline infix def -=(compType: ComponentType)(using world: World): Entity =
+        world.removeComponent(e, compType)
+        e
 
-    inline infix def has(compType: ComponentType)(using world: World): Boolean =
-      world.hasComponent(e, compType)
+      inline infix def has(compType: ComponentType)(using world: World): Boolean =
+        world.hasComponent(e, compType)
 
-    inline infix def has(compTypes: ComponentType*)(using world: World): Boolean =
-      require(compTypes.nonEmpty, "Given type sequence cannot be empty.")
-      compTypes forall has
+      inline infix def has(compTypes: ComponentType*)(using world: World): Boolean =
+        require(compTypes.nonEmpty, "Given type sequence cannot be empty.")
+        compTypes forall has
