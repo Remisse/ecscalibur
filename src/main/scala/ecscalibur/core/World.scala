7c390fbe1fbaebbdcb6a970015be09ec6c94f47f
Bryan Corradino <zeromuseg7@gmail.com>
2024-06-22 12:53:16 +0200

    Fix World to reflect recent Component changes

--- src/main/scala/ecscalibur/core/World.scala
+++ src/main/scala/ecscalibur/core/World.scala
@@ -25,24 +25,24 @@ object Worlds:
       if !components.contains(id) then components += ArrayBuffer.empty
       Entity(id)
 
-    override inline def isValid(e: Entity): Boolean = idGenerator.isValid(e.id)
+    override def isValid(e: Entity): Boolean = idGenerator.isValid(e.id)
 
-    override inline def addComponent[T <: Component](e: Entity, c: T) =
-      if hasComponent(e, c.tpe) then
+    override def addComponent[T <: Component](e: Entity, c: T) =
+      if hasComponent(e, c.typeId) then
         throw IllegalArgumentException(s"Entity $this already has a component of type ${c.getClass}.")
       components(e.id) += c
 
     private inline def hasComponent(e: Entity, id: ComponentId): Boolean =
       findComponentIdx(e, id) != -1
 
-    override inline def removeComponent(e: Entity, compType: ComponentType): Unit =
+    override def removeComponent(e: Entity, compType: ComponentType): Unit =
       val i = findComponentIdx(e, ~compType)
       if i == -1 then
         throw new IllegalArgumentException(s"Entity $this does not have a component of type ${compType.getClass}.")
-      components(e.id).remove(i)
+      val _ = components(e.id).remove(i)
 
-    override inline def hasComponent(e: Entity, compType: ComponentType): Boolean =
+    override def hasComponent(e: Entity, compType: ComponentType): Boolean =
       findComponentIdx(e, ~compType) != -1
 
     private inline def findComponentIdx(e: Entity, id: ComponentId): Int =
-      components(e.id).indexWhere(_.tpe == id)
+      components(e.id).indexWhere(_.typeId == id)
