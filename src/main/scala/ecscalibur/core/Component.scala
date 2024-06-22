6e895fb0bd0ff0638bb5ccd0db153ea20a49d6d8
Bryan Corradino <zeromuseg7@gmail.com>
2024-06-22 13:10:42 +0200

    Introduce opaque type alias for Array[Component]

--- src/main/scala/ecscalibur/core/Component.scala
+++ src/main/scala/ecscalibur/core/Component.scala
@@ -30,10 +30,33 @@ object Components:
       case o: ComponentType => typeId == o.typeId
       case _                => false
 
+  object CSeqs:
+    import scala.reflect.ClassTag
+
+    opaque type CSeq = Array[Component]
+    inline def <<[T <: Component: ClassTag](using l: CSeq): T = l.get[T]
+
+    object CSeq:
+      def apply(comps: Component*): CSeq = comps.toArray
+      def apply(comps: Array[Component]): CSeq = comps
+      def apply(comps: Iterable[Component]): CSeq = comps.toArray
+
+    extension (l: CSeq)
+      inline def underlying: Array[Component] = l
+      inline def toTypes: Array[ComponentId] = l.map(_.typeId)
+      inline def get[T <: Component: ClassTag]: T =
+        val idx = l.indexWhere {
+          case t: T => true
+          case _    => false
+        }
+        if idx == -1 then
+          throw IllegalArgumentException(s"No component of class ${summon[ClassTag[T]]} found.")
+        l(idx).asInstanceOf[T]
+
+  export TypeOrdering.given
   object TypeOrdering:
     given Ordering[ComponentType] with
-      def compare(t1: ComponentType, t2: ComponentType): Int = ~t1 - ~t2
-  export TypeOrdering.given_Ordering_ComponentType
+      override def compare(t1: ComponentType, t2: ComponentType): Int = ~t1 - ~t2
 
   object Annotations:
     import scala.annotation.MacroAnnotation
