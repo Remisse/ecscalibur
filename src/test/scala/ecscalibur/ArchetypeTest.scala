505a393b7602a10c2da0f385c0580cbde2517ef0
Bryan Corradino <zeromuseg7@gmail.com>
2024-06-22 18:35:04 +0200

    Slightly improve Archetype tests

--- src/test/scala/ecscalibur/ArchetypeTest.scala
+++ src/test/scala/ecscalibur/ArchetypeTest.scala
@@ -43,8 +43,8 @@ class ArchetypeTest extends AnyFlatSpec with should.Matchers:
     an[IllegalArgumentException] shouldBe thrownBy(Archetype(C1, C1, C2))
 
   @component
-  case class WithValue(val x: Int) extends Component
-  object WithValue extends ComponentType
+  case class Value(val x: Int) extends Component
+  object Value extends ComponentType
 
   it should "correctly store entities and their components" in:
     val e1 = Entity(0)
@@ -54,10 +54,10 @@ class ArchetypeTest extends AnyFlatSpec with should.Matchers:
 
   it should "not accept entities that do not satisfy its signature" in:
     val e1 = Entity(0)
-    val archetype = Archetype(WithValue, C2)
+    val archetype = Archetype(Value, C2)
     an[IllegalArgumentException] shouldBe thrownBy(archetype.add(e1, CSeq(C2())))
     an[IllegalArgumentException] shouldBe thrownBy(
-      archetype.add(e1, CSeq(WithValue(1), C2(), C3()))
+      archetype.add(e1, CSeq(Value(1), C2(), C3()))
     )
 
   it should "correctly remove stored entities" in:
@@ -69,8 +69,8 @@ class ArchetypeTest extends AnyFlatSpec with should.Matchers:
 
   it should "return all components of an entity when that entity is removed" in:
     val e1 = Entity(0)
-    val archetype = Archetype(WithValue, C1, C2)
-    val wv = WithValue(3)
+    val archetype = Archetype(Value, C1, C2)
+    val wv = Value(3)
     val c1 = C1() 
     val c2 = C2()
     archetype.add(e1, CSeq(wv, c1, c2))
@@ -85,34 +85,38 @@ class ArchetypeTest extends AnyFlatSpec with should.Matchers:
     archetype.contains(e1) shouldBe false
 
   it should "correctly iterate over all selected entities and components in read-only mode" in:
-    val arch = Archetype(WithValue, C2)
-    val e1 = Entity(0)
-    val wv = WithValue(5)
-    arch.add(e1, CSeq(wv, C2()))
-    arch.readAll(
-      _ == ~WithValue, 
-      (e, comps) =>
-        val c = comps.get[WithValue]
-        c shouldBe wv
-        val _ = an[IllegalArgumentException] shouldBe thrownBy(comps.get[C1])
+    val arch = Archetype(Value, C2)
+    val (v1, v2) = (Value(1), Value(2))
+    val toAdd: Map[Entity, CSeq] = Map(
+      Entity(0) -> CSeq(v1, C2()), Entity(1) -> CSeq(v2, C2())
     )
+    for (entity, comps) <- toAdd do arch.add(entity, comps)
+    var sum = 0
+    arch.readAll(
+      _ == ~Value, 
+      (e, comps) =>
+        val c = comps.get[Value]
+        an[IllegalArgumentException] shouldBe thrownBy(comps.get[C1])
+        sum += c.x
+    )
+    sum shouldBe (v1.x + v2.x)
 
   it should "correctly iterate over all selected entities and components in RW mode" in:
-    val arch = Archetype(WithValue, C2)
+    val arch = Archetype(Value, C2)
     val e1 = Entity(0)
-    val wv = WithValue(5)
-    val editedWv = WithValue(0)
+    val wv = Value(5)
+    val editedWv = Value(0)
     arch.add(e1, CSeq(wv, C2()))
     arch.writeAll(
-      _ == ~WithValue, 
+      _ == ~Value, 
       (e, comps) =>
         given CSeq = comps
-        val c = <<[WithValue] // Equivalent to comps.get[WithValue]
+        val c = <<[Value] // Equivalent to comps.get[Value]
         c shouldBe wv
         CSeq(editedWv)
     )
     arch.readAll(
-      _ == ~WithValue, 
+      _ == ~Value, 
       (e, comps) =>
-        val _ = comps.get[WithValue] shouldBe editedWv
+        val _ = comps.get[Value] shouldBe editedWv
     )
