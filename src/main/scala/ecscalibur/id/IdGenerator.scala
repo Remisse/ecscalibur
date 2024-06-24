536dee2099c72bb79929945d3d3b2b0027805730
Bryan Corradino <zeromuseg7@gmail.com>
2024-06-24 13:02:53 +0200

    Use a set to track erased IDs instead of an ArrayBuffer

--- src/main/scala/ecscalibur/id/IdGenerator.scala
+++ src/main/scala/ecscalibur/id/IdGenerator.scala
@@ -10,16 +10,18 @@ object IdGenerator:
 
 private class IdGeneratorImpl extends IdGenerator:
   private var highestAvailableIdx = 0
-  import scala.collection.mutable.ArrayBuffer
-  private val erasedIds = ArrayBuffer.empty[Int]
+  import scala.collection.mutable
+  private val erasedIds = mutable.HashSet.empty[Int]
 
   override def next: Int =
     var res: Int = -1
-    erasedIds.length match
+    erasedIds.size match
       case 0 =>
         res = highestAvailableIdx
         highestAvailableIdx = highestAvailableIdx + 1
-      case _ => res = erasedIds.remove(erasedIds.length - 1)
+      case _ => 
+        res = erasedIds.head
+        erasedIds -= res
     res
 
   override def erase(id: Int): Boolean =
