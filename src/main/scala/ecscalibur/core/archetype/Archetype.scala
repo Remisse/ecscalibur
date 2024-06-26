236cea183f8f865e635d7b8abe0195b6142cefbe
Bryan Corradino <zeromuseg7@gmail.com>
2024-06-26 21:16:59 +0200

    Split Archetype into Aggregate and Fragment

--- src/main/scala/ecscalibur/core/archetype/Archetype.scala
+++ src/main/scala/ecscalibur/core/archetype/Archetype.scala
@@ -4,16 +4,15 @@ import ecscalibur.core.component.*
 import CSeq.Extensions.*
 import ecscalibur.core.Entity
 import ecscalibur.id.IdGenerator
+import ecscalibur.util.sizeOf.sizeOf
 
+import scala.annotation.targetName
 import scala.collection.mutable
-import scala.collection.mutable.ArrayBuffer
 import scala.collection.immutable.*
 import scala.reflect.ClassTag
-import scala.annotation.targetName
 
 private[core] object Archetypes:
-  trait Archetype:
-    def signature: Signature
+  trait Archetype private[archetype] (val signature: Signature):
     def add(e: Entity, entityComponents: CSeq): Unit
     def contains(e: Entity): Boolean
     def remove(e: Entity): CSeq
@@ -22,74 +21,165 @@ private[core] object Archetypes:
     def writeAll(predicate: ComponentId => Boolean)(f: (Entity, CSeq) => CSeq): Unit
 
   object Archetype:
+    // TODO Make this parameter configurable
+    inline val DefaultFragmentSizeBytes = 16384
     @targetName("fromTypes")
-    def apply(types: ComponentType*): Archetype = apply(Signature(types*))
+    def apply(types: ComponentType*): Aggregate = Aggregate(DefaultFragmentSizeBytes, Signature(types*))
     @targetName("fromSignature")
-    def apply(signature: Signature): Archetype = ArchetypeImpl(signature)
+    def apply(signature: Signature): Aggregate = Aggregate(DefaultFragmentSizeBytes, signature)
 
-    private class ArchetypeImpl(inSignature: Signature) extends Archetype:
-      import ecscalibur.core.util.array.*
+  trait Aggregate extends Archetype:
+    def fragments: Iterable[Fragment]
 
-      private val _signature: Signature = inSignature
-      private val compIndexesByEntity: mutable.Map[Entity, Int] = mutable.HashMap.empty
-      private val components: Map[ComponentId, ArrayBuffer[Component]] =
-        _signature.underlying.aMap(t => t -> ArrayBuffer.empty[Component]).to(HashMap)
-      private val idGenerator: IdGenerator = IdGenerator()
+  trait Fragment extends Archetype:
+    def isFull: Boolean
+    def isEmpty: Boolean
 
-      override inline def signature: Signature = _signature
+  object Aggregate:
+    @targetName("fromTypes")
+    def apply(maxFragmentSizeBytes: Long, types: ComponentType*): Aggregate = apply(maxFragmentSizeBytes, Signature(types*))
+    @targetName("fromSignature")
+    def apply(maxFragmentSizeBytes: Long, signature: Signature): Aggregate = AggregateImpl(signature, maxFragmentSizeBytes)
+
+    private class AggregateImpl(inSignature: Signature, maxFragmentSizeBytes: Long) extends Archetype(inSignature), Aggregate:
+      import ecscalibur.util.array.*
+
+      private var _fragments: Vector[Fragment] = Vector.empty
+      private val entitiesByFragment: mutable.Map[Entity, Fragment] = mutable.Map.empty
+
+      override def fragments: Iterable[Fragment] = _fragments
 
       override def add(e: Entity, entityComponents: CSeq): Unit =
         require(!contains(e), "Attempted to readd an already existing entity.")
         require(
-          _signature == Signature(entityComponents.toTypes),
+          signature == Signature(entityComponents.toTypes),
           "Given component types do not correspond to this archetype's signature."
         )
-        val newEntityIdx = idGenerator.next
-        compIndexesByEntity += e -> newEntityIdx
-        entityComponents.underlying.aForeach: (c: Component) =>
-          val compBuffer = components(c.typeId)
-          if newEntityIdx >= compBuffer.length then compBuffer += c
-          else compBuffer(newEntityIdx) = c
+        if (_fragments.isEmpty || _fragments.head.isFull)
+          val sizeBytes = estimateComponentsSize(entityComponents)
+          if (sizeBytes > maxFragmentSizeBytes)
+            throw IllegalStateException(
+              s"Exceeded the maximum fragment size ($sizeBytes > $maxFragmentSizeBytes)."
+            )
+          val maxEntities = (maxFragmentSizeBytes / sizeBytes).toInt
+          _fragments = Fragment(signature, maxEntities) +: _fragments
+        _fragments.head.add(e, entityComponents)
+        entitiesByFragment += e -> _fragments.head
 
-      override inline def contains(e: Entity): Boolean = 
-        compIndexesByEntity.contains(e) && idGenerator.isValid(compIndexesByEntity(e))
+      private inline def estimateComponentsSize(components: CSeq): Long =
+        var estimatedComponentsSize: Long = 0
+        components.underlying.aForeach(c => estimatedComponentsSize += sizeOf(c))
+        estimatedComponentsSize
+
+      override inline def contains(e: Entity): Boolean = entitiesByFragment.contains(e)
 
       inline val removalErrorMsg = "Attempted to remove an entity not stored in this archetype."
 
       override def remove(e: Entity): CSeq =
         require(contains(e), removalErrorMsg)
-        val idx = compIndexesByEntity(e)
+        val fragment = entitiesByFragment(e)
+        entitiesByFragment -= e
+        val res = fragment.remove(e)
+        maybeDeleteFragment(fragment)
+        res
+
+      override def softRemove(e: Entity) =
+        require(contains(e), removalErrorMsg)
+        val fragment = entitiesByFragment(e)
+        entitiesByFragment -= e
+        fragment.softRemove(e)
+        maybeDeleteFragment(fragment)
+      
+      private inline def maybeDeleteFragment(fr: Fragment) =
+        if (fr.isEmpty && fr != _fragments.head) 
+          _fragments = _fragments.filterNot(_ == fr)
+
+      override def readAll(predicate: ComponentId => Boolean)(f: (Entity, CSeq) => Unit) =
+        _fragments.foreach(fr => fr.readAll(predicate)(f))
+
+      override def writeAll(predicate: ComponentId => Boolean)(f: (Entity, CSeq) => CSeq) =
+        _fragments.foreach(fr => fr.writeAll(predicate)(f))
+
+      override def equals(x: Any): Boolean = x match
+        case a: Archetype => signature == a.signature
+        case _            => false
+
+      override def hashCode(): Int = signature.hashCode
+
+  object Fragment:
+    def apply(signature: Signature, maxEntities: Int): Fragment =
+      FragmentImpl(signature, maxEntities)
+
+    private class FragmentImpl(inSignature: Signature, maxEntities: Int)
+        extends Archetype(inSignature),
+          Fragment:
+      import ecscalibur.util.array.*
+
+      private val idGenerator: IdGenerator = IdGenerator()
+      private val entityIndexes: mutable.Map[Entity, Int] = new mutable.HashMap(maxEntities, FragmentImpl.LoadFactor)
+      private val components: Map[ComponentId, CSeq] =
+        signature.underlying.aMap(t => t -> CSeq(Array.ofDim[Component](maxEntities))).to(HashMap)
+
+      override inline def isFull: Boolean = entityIndexes.size == maxEntities
+
+      override def isEmpty: Boolean = entityIndexes.isEmpty
+
+      override def add(e: Entity, entityComponents: CSeq): Unit =
+        // No need to validate the inputs, as Archetype already takes care of it.
+        require(!isFull, s"Cannot add more entities beyond the maximum limit ($maxEntities).")
+        val newEntityIdx = idGenerator.next
+        entityIndexes += e -> newEntityIdx
+        entityComponents.underlying.aForeach: (c: Component) =>
+          components(c.typeId)(newEntityIdx) = c
+
+      override inline def contains(e: Entity): Boolean =
+        entityIndexes.contains(e)
+
+      inline val removalErrorMsg = "Attempted to remove an entity not stored in this archetype."
+
+      override def remove(e: Entity): CSeq =
+        val idx = removeEntityFromMap(e)
         idGenerator.erase(idx) match
           case false => throw new IllegalArgumentException(removalErrorMsg)
           case _     => CSeq(components.map((_, comps) => comps(idx)))
 
       override def softRemove(e: Entity) =
-        require(contains(e), removalErrorMsg)
-        val idx = compIndexesByEntity(e)
+        val idx = removeEntityFromMap(e)
         val _ = idGenerator.erase(idx)
+
+      private inline def removeEntityFromMap(e: Entity): Int =
+        val idx = entityIndexes(e)
+        entityIndexes -= e
+        idx
 
       override def readAll(predicate: ComponentId => Boolean)(f: (Entity, CSeq) => Unit) =
         val filteredComps = components.filter((id, _) => predicate(id))
-        for (e, idx) <- compIndexesByEntity if contains(e) do
+        for (e, idx) <- entityIndexes do
           val inputComps = CSeq(filteredComps.map((_, comps) => comps(idx)))
           f(e, inputComps)
 
+      // TODO RefRW as with DOTS
       override def writeAll(predicate: ComponentId => Boolean)(f: (Entity, CSeq) => CSeq) =
-        val filteredComps = components.filter((id, _) => predicate(id))
-        val inputIds = filteredComps.map((id, _) => id).toArray
-        for (e, idx) <- compIndexesByEntity if contains(e) do
-          val inputComps = CSeq(filteredComps.map((_, comps) => comps(idx)))
-          val editedComponents: CSeq = f(e, inputComps)
-          val returnedSignature = editedComponents.underlying.toSignature
+        val filteredComps = components.collect { case (id, a) if predicate(id) => a }
+        for (e, idx) <- entityIndexes do
+          // TODO Rewrite this using recursion
+          val inputComps = Array.ofDim[Component](filteredComps.size)
+          var i = 0
+          filteredComps.foreach: a =>
+            inputComps(i) = a(idx)
+            i += 1
+          val editedComponents: CSeq = f(e, CSeq(inputComps))
+          val returnedSignature =
+            if editedComponents.underlying.isEmpty then Signature.nil
+            else editedComponents.underlying.toSignature
+          val inputIds = filteredComps.map(a => a(0).typeId).toArray
           require(
-            returnedSignature == inputIds.toSignature,
+            // TODO Replace this equality check with 'containsAll' and delete 'readAll'
+            returnedSignature == Signature(inputIds),
             s"Unexpected components returned.\nExpected: ${inputIds.mkString}\nFound: ${returnedSignature.underlying.mkString}"
           )
           editedComponents.underlying.aForeach: (c) =>
-            components(c.typeId)(compIndexesByEntity(e)) = c
+            components(c.typeId)(entityIndexes(e)) = c
 
-      override def equals(x: Any): Boolean = x match
-        case a: Archetype => _signature == a.signature
-        case _            => false
-
-      override def hashCode(): Int = _signature.hashCode
+    object FragmentImpl:
+      val LoadFactor = 0.7
--- src/test/scala/ecscalibur/ArchetypeTest.scala
+++ src/test/scala/ecscalibur/ArchetypeTest.scala
@@ -6,63 +6,69 @@ import org.scalatest.matchers.*
 import component.*
 import CSeq.Extensions.*
 import component.Annotations.component
-import ecscalibur.core.archetype.Archetypes.Archetype
+import ecscalibur.core.archetype.Archetypes.Aggregate
 import ecscalibur.core.archetype.Signature
+import ecscalibur.core.archetype.Archetypes.Fragment
+import ecscalibur.util.sizeOf.sizeOf
+
+@component
+class C1 extends Component
+object C1 extends ComponentType
+
+@component
+class C2 extends Component
+object C2 extends ComponentType
+
+@component
+class C3 extends Component
+object C3 extends ComponentType
+
+@component
+case class Value(val x: Int) extends Component
+object Value extends ComponentType
 
 class ArchetypeTest extends AnyFlatSpec with should.Matchers:
-  @component
-  class C1 extends Component
-  object C1 extends ComponentType
+  inline val DefaultFragmentSizeBytes = 16384
+  inline val KindaSmallFragmentSizeBytes = 64
+  inline val ExtremelySmallFragmentSizeBytes = 1
 
-  @component
-  class C2 extends Component
-  object C2 extends ComponentType
+  "An archetype Aggregate with no signature" should "throw when created" in:
+    an[IllegalArgumentException] should be thrownBy (Aggregate(DefaultFragmentSizeBytes))
 
-  @component
-  class C3 extends Component
-  object C3 extends ComponentType
-
-  "An archetype with no signature" should "throw when created" in:
-    an[IllegalArgumentException] shouldBe thrownBy(Archetype())
-
-  "An archetype" should "be identified by the component classes it holds" in:
-    val archetype = Archetype(C1, C2)
+  "An Aggregate archetype" should "be identified by the component classes it holds" in:
+    val archetype = Aggregate(DefaultFragmentSizeBytes, C1, C2)
     archetype.signature shouldBe Signature(C1, C2)
     archetype.signature shouldBe Signature(C2, C1)
     archetype.signature shouldNot be(Signature(C1))
     archetype.signature shouldNot be(Signature(C1, C2, C3))
 
   it should "have a signature made of distinct component types only" in:
-    an[IllegalArgumentException] shouldBe thrownBy(Archetype(C1, C1, C2))
-
-  @component
-  case class Value(val x: Int) extends Component
-  object Value extends ComponentType
+    an[IllegalArgumentException] shouldBe thrownBy(Aggregate(DefaultFragmentSizeBytes, C1, C1, C2))
 
   it should "correctly store entities and their components" in:
     val e1 = Entity(0)
-    val archetype = Archetype(C1, C2)
+    val archetype = Aggregate(DefaultFragmentSizeBytes, C1, C2)
     archetype.add(e1, CSeq(C1(), C2()))
     archetype.contains(e1) shouldBe true
 
   it should "not accept entities that do not satisfy its signature" in:
     val e1 = Entity(0)
-    val archetype = Archetype(Value, C2)
-    an[IllegalArgumentException] shouldBe thrownBy(archetype.add(e1, CSeq(C2())))
-    an[IllegalArgumentException] shouldBe thrownBy(
+    val archetype = Aggregate(DefaultFragmentSizeBytes, Value, C2)
+    an[IllegalArgumentException] should be thrownBy (archetype.add(e1, CSeq(C2())))
+    an[IllegalArgumentException] should be thrownBy (
       archetype.add(e1, CSeq(Value(1), C2(), C3()))
     )
 
   it should "correctly remove stored entities" in:
     val e1 = Entity(0)
-    val archetype = Archetype(C1)
+    val archetype = Aggregate(DefaultFragmentSizeBytes, C1)
     archetype.add(e1, CSeq(C1()))
     archetype.remove(e1)
     archetype.contains(e1) shouldBe false
 
   it should "return all components of an entity when that entity is removed" in:
     val e1 = Entity(0)
-    val archetype = Archetype(Value, C1, C2)
+    val archetype = Aggregate(DefaultFragmentSizeBytes, Value, C1, C2)
     val wv = Value(3)
     val c1 = C1()
     val c2 = C2()
@@ -71,14 +77,14 @@ class ArchetypeTest extends AnyFlatSpec with should.Matchers:
 
   it should "correctly soft-remove entities" in:
     val e1 = Entity(0)
-    val archetype = Archetype(C1)
+    val archetype = Aggregate(DefaultFragmentSizeBytes, C1)
     val c1 = C1()
     archetype.add(e1, CSeq(c1))
     archetype.softRemove(e1)
     archetype.contains(e1) shouldBe false
 
   it should "correctly iterate over all selected entities and components in read-only mode" in:
-    val arch = Archetype(Value, C2)
+    val arch = Aggregate(DefaultFragmentSizeBytes, Value, C2)
     val (v1, v2) = (Value(1), Value(2))
     val toAdd: Map[Entity, CSeq] = Map(
       Entity(0) -> CSeq(v1, C2()),
@@ -88,12 +94,12 @@ class ArchetypeTest extends AnyFlatSpec with should.Matchers:
     var sum = 0
     arch.readAll(_ == ~Value): (e, comps) =>
       val c = comps.get[Value]
-      an[IllegalArgumentException] shouldBe thrownBy(comps.get[C1])
+      an[IllegalArgumentException] should be thrownBy (comps.get[C1])
       sum += c.x
     sum shouldBe (v1.x + v2.x)
 
   it should "correctly iterate over all selected entities and components in RW mode" in:
-    val arch = Archetype(Value, C2)
+    val arch = Aggregate(DefaultFragmentSizeBytes, Value, C2)
     val e1 = Entity(0)
     val wv = Value(5)
     val editedWv = Value(0)
@@ -105,3 +111,55 @@ class ArchetypeTest extends AnyFlatSpec with should.Matchers:
       CSeq(editedWv)
     arch.readAll(_ == ~Value): (e, comps) =>
       val _ = comps.get[Value] shouldBe editedWv
+
+  it should "correctly perform load balancing when fragments reach their limit" in:
+    // sizeOf incorrectly reports sizes greater than 4900 bytes for classes
+    // declared within test classes.
+    val arch = Aggregate(KindaSmallFragmentSizeBytes, Value)
+    val components = CSeq(Value(0))
+    inline val numberOfEntities = 1000
+    for i <- (0 until numberOfEntities) do
+      noException should be thrownBy (arch.add(Entity(i), components))
+
+  it should "correctly return its Fragments" in:
+    val arch = Aggregate(KindaSmallFragmentSizeBytes, C1)
+    arch.fragments.isEmpty shouldBe true
+    val c = C1()
+    inline val numberOfEntities = 1000
+    for i <- (0 until numberOfEntities) do arch.add(Entity(i), CSeq(c))
+    val expectedNumberOfFragments = numberOfEntities / (KindaSmallFragmentSizeBytes / sizeOf(c))
+    arch.fragments.size shouldBe expectedNumberOfFragments
+
+  it should "remove all empty Fragments but one" in:
+    val arch = Aggregate(KindaSmallFragmentSizeBytes, C1)
+    val c = C1()
+    inline val numberOfEntities = 1000
+    val entities = (0 until numberOfEntities).map(Entity(_))
+    for e <- entities do arch.add(e, CSeq(c))
+    for e <- entities do arch.remove(e)
+    arch.fragments.size shouldBe 1
+
+  it should "throw if the size of a component is greather than the maximum size limit" in:
+    val arch = Aggregate(ExtremelySmallFragmentSizeBytes, Value)
+    val components = CSeq(Value(0))
+    an[IllegalStateException] should be thrownBy (arch.add(Entity(0), components))
+
+  "A Fragment" should "correctly report whether it is full or not" in:
+    inline val maxEntities = 1
+    val frag = Fragment(Signature(Value), maxEntities)
+    val e = Entity(0)
+    frag.isEmpty shouldBe true
+    frag.isFull shouldBe !frag.isEmpty
+    frag.add(e, CSeq(Value(0)))
+    frag.isEmpty shouldBe false
+    frag.isFull shouldBe !frag.isEmpty
+    frag.remove(e)
+    frag.isEmpty shouldBe true
+    frag.isFull shouldBe !frag.isEmpty
+
+  it should "throw when attempting to add more entities than it can store" in:
+    inline val maxEntities = 1
+    val frag = Fragment(Signature(Value), maxEntities)
+    val v = Value(0)
+    noException should be thrownBy (frag.add(Entity(0), CSeq(v)))
+    an[IllegalArgumentException] should be thrownBy (frag.add(Entity(1), CSeq(v)))
