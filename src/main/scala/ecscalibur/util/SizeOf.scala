dbfd4119ad30e0750bb27a49abdcbccd74d537a0
Bryan Corradino <zeromuseg7@gmail.com>
2024-06-26 21:14:31 +0200

    Add wrapper around SizeEstimator to be used on Components

---
+++ src/main/scala/ecscalibur/util/SizeOf.scala
@@ -0,0 +1,14 @@
+package ecscalibur.util
+
+import ecscalibur.core.component.{ComponentId, Component}
+import ecscalibur.util.spark.SizeEstimator
+
+object sizeOf:
+  private var cache: Map[ComponentId, Long] = Map.empty
+
+  def sizeOf(c: Component): Long = 
+    if (cache.contains(~c)) cache(~c)
+    else
+      val size = SizeEstimator.estimate(c)
+      cache = cache + (~c -> size)
+      size
